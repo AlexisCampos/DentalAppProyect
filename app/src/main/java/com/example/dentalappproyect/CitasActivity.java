b25f2bf128b65ee628655df66156ff1cb9dfe86a
AlexisCampos <camposlopezalexis@gmail.com>
2019-11-23 19:53:45 -0800

    Se borro el boton de opciones de citas

--- app/src/main/java/com/example/dentalappproyect/CitasActivity.java
+++ app/src/main/java/com/example/dentalappproyect/CitasActivity.java
@@ -9,21 +9,11 @@ import android.widget.Button;
 
 public class CitasActivity extends AppCompatActivity {
 
-    private Button btnToOpciones;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_citas);
 
-        btnToOpciones=(Button)findViewById(R.id.btnToOpciones);
-
-        btnToOpciones.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i =new Intent(CitasActivity.this,OpcionesActivity.class);
-                startActivity(i);
-            }
-        });
     }
 }
